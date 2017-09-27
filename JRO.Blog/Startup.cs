1b8ebdbc9e5dc3894dce35255cffe01db66f4cdc
jro.devops <jro.devops@gmail.com>
2017-09-27 12:20:29 +0200

    Add project files.

---
+++ JRO.Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JRO.Blog.Startup))]
+namespace JRO.Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
